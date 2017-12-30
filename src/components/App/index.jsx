1f363ee5d3c0479bbcf4987c1a1fe63dc77beeb9
Caroline Benn <lineybenn@gmail.com>
2017-12-30 22:58:19 +0000

    Remove Main and Header components

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
-// import Header from '../Header';
-import Main from '../Main';
+import { HomepageContainer } from '../Homepage';
+import Photos from '../Photos';
+import { PhotoPageContainer } from '../PhotoPage';
+import TakeAPhoto from '../TakeAPhoto';
+
+import { Switch, Route } from 'react-router-dom';
 
 import './styles.css';
 
@@ -9,8 +13,14 @@ class App extends Component {
   render() {
     return (
       <div className="App-container">
-        {/*<Header />*/}
-        <Main />
+        <main>
+        <Switch>
+          <Route exact path='/' component={HomepageContainer}/>
+          <Route path='/take-photo' component={TakeAPhoto}/>
+          <Route path='/photos/:id' component={PhotoPageContainer}/>
+          <Route path='/photos' component={Photos}/>
+        </Switch>
+      </main>
       </div>
     );
   }
